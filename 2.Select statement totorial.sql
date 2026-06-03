f38411bbb90204b4848c71884c9c757a81a3a971
Roshan246810 <roshanjora88@gmail.com>
2026-06-03 21:32:56 +0545

    Add files via upload

---
+++ 2.Select statement totorial.sql
@@ -0,0 +1,11 @@
+select *
+from employee_demographics;
+select first_name,
+last_name,
+age
+from employee_demographics
+
+#pendas
+select distinct first_name,
+gender
+from parks_and_recreation.employee_demographics;
---
+++ 3.Where clause.sql
@@ -0,0 +1,8 @@
+
+# Where clause
+Select * from employee_salary
+where salary>=50000;
+
+select * from employee_demographics
+where gender !='Female'
+;
