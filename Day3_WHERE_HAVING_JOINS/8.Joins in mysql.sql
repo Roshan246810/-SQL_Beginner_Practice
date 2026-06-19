40d2a7b6316080348ba9caaaf1ec59b737fedf80
Roshan246810 <roshanjora88@gmail.com>
2026-06-19 11:16:03 +0545

    Update 8.Joins in mysql.sql

--- Day3_WHERE_HAVING_JOINS/8.Joins in mysql.sql
+++ Day3_WHERE_HAVING_JOINS/8.Joins in mysql.sql
@@ -10,13 +10,13 @@ FROM parks_and_recreation.employee_salary
 INNER JOIN parks_and_recreation.employee_demographics
     ON employee_demographics.employee_id = employee_salary.employee_id;
 
--- LEFT JOIN (your code was correct ✅)
+-- LEFT JOIN
 SELECT *
 FROM employee_demographics
 LEFT OUTER JOIN employee_salary
     ON employee_demographics.first_name = employee_salary.first_name;
 
--- RIGHT JOIN (your code was correct ✅)
+-- RIGHT JOIN
 SELECT *
 FROM employee_demographics
 RIGHT OUTER JOIN employee_salary
